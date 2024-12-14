c4ea70d0d7b34d0ad4a16f3a8ecb045bfee39bb4
BlackfireZZZ <Stipundrik@gmail.com>
2024-12-14 19:19:54 +0300

    Proto generated

--- deployer/internal/migrations_module/migrations/00001_services.sql
+++ deployer/internal/migrations_module/migrations/00001_services.sql
@@ -1,15 +1,22 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS service (
-    id uuid PRIMARY KEY,
-    ssh_port INTEGER NOT NULL,
-    web_port INTEGER NOT NULL
+    id uuid PRIMARY KEY
 );
+
+CREATE TABLE IF NOT EXISTS instance (
+    id uuid PRIMARY KEY,
+    ssh_port int NOT NULL,
+    web_port int NOT NULL,
+    service_id uuid REFERENCES service(id)
+);
+
 -- +goose StatementBegin
 SELECT 'up SQL query';
 -- +goose StatementEnd
 
 -- +goose Down
 DROP TABLE IF EXISTS service;
+DROP TABLE IF EXISTS instance;
 -- +goose StatementBegin
 SELECT 'down SQL query';
 -- +goose StatementEnd
